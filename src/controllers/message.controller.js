7deac317aac5b9b7af7e2fe51e9d3118f752aa63
gopik2230 <gopik2230@gmail.com>
2025-04-02 23:15:52 +0530

    'startSendMessageApi'

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -16,13 +16,25 @@ export const getUsersForSidebar = async(req,res) => {
 export const getMessages = async(req,res) => {
     try {
         const { id:userToChatId } = req.params
-        const senderId = req.user._id
+        const myId = req.user._id
 
         const messages = await Message.find({
             $or:[
-                
+                {senderId:myId, receiverId:userToChatId},
+                {senderId:userToChatId, receiverId:myId}
             ]
         })
+
+        res.status(200).json(messages)
+    } catch (error) {
+        console.log("Error in getMessages controller "+error.message)
+        res.status(500).json({ message: "Internal Server Error" })
+    }
+}
+
+export const sendMessage = async(req,res) => {
+    try {
+        
     } catch (error) {
         
     }
