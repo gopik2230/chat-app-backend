a6351cb50f3ba0a1f6a8f76aa9c2e5cf146233fa
gopik2230 <gopik2230@gmail.com>
2025-04-03 23:05:58 +0530

    'latestCode'

--- src/controllers/message.controller.js
+++ src/controllers/message.controller.js
@@ -1,3 +1,4 @@
+import cloudinary from "../lib/cloudinary.js"
 import Message from "../models/message.model.js"
 import User from "../models/user.model.js"
 
@@ -34,9 +35,31 @@ export const getMessages = async(req,res) => {
 
 export const sendMessage = async(req,res) => {
     try {
-        
+        const { text, image } = req.body
+        const { id: receiverId } = req.params
+        const senderId = req.user._id
+
+        let imageUrl;
+        if(image) {
+            //Upload base64 image to cloudinary
+            const uploadedResponse = await cloudinary.uploader.upload(image)
+            imageUrl = uploadedResponse?.secure_url
+        }
+
+        const newMessage = new Message({
+            senderId,
+            receiverId,
+            text,
+            image: imageUrl
+        })
+
+        await newMessage.save()
+        //realtime  socket.io soon
+
+        res.status(201).json(newMessage)
     } catch (error) {
-        
+        console.log("Error in sendMessage controller "+error.message)
+        res.status(500).json({ message: "Internal Server Error" })
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv"
 import { connecDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors"
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
@@ -14,6 +15,12 @@ const PORT = process.env.PORT
 
 app.use(express.json()) //extract json data out of body
 app.use(cookieParser()) //allow us to parse the cookie
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+
+}
+))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/message", messageRoutes)
