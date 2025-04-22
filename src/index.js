5da15e48742a94322d6a2d30e41b6df5662d7939
gopik2230 <gopik2230@gmail.com>
2025-04-22 20:30:22 +0530

    sdf

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors"
 
-import path from "path"
+
 
 import authRoutes from './routes/auth.route.js'
 import messageRoutes from './routes/message.route.js'
@@ -14,12 +14,12 @@ dotenv.config();
 
 
 const PORT = process.env.PORT
-const __dirname = path.resolve()
+
 
 app.use(express.json()) //extract json data out of body
 app.use(cookieParser()) //allow us to parse the cookie
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: true,
     credentials: true
 
 }
@@ -28,12 +28,6 @@ app.use(cors({
 app.use("/api/auth", authRoutes)
 app.use("/api/messages", messageRoutes)
 
-if(process.env.NODE_ENV==="production") {
-    app.use(express.static(path.join(__dirname, "../frontend/dist")))
-    app.get("*", (req,res) => {
-        res.sendFile(path.join(__dirname, "../frontend","dist","index.html"))
-    })
-}
 
 server.listen(PORT, () => {
     console.log("Server is running on PORT "+ PORT);
